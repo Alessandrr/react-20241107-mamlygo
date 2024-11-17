1ccf656e9a2cd683a1102d61716cbe068998576e
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2024-11-17 16:39:51 +0400

    refactor onChange callback in restaurantTabs.jsx

--- src/components/restaurantTabs/restaurantTabs.jsx
+++ src/components/restaurantTabs/restaurantTabs.jsx
@@ -1,17 +1,10 @@
 import { restaurants } from '../../mocks/restaurants.js';
 
 export const RestaurantTabs = ({ onChange }) => {
-  const handleTabSelected = (restaurant) => {
-    onChange(restaurant);
-  };
-
   return (
     <div>
       {restaurants.map((restaurant) => (
-        <button
-          key={restaurant.id}
-          onClick={() => handleTabSelected(restaurant)}
-        >
+        <button key={restaurant.id} onClick={() => onChange(restaurant)}>
           {restaurant.name}
         </button>
       ))}
